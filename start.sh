ad3dd3770f520548c72650e2b99260d10d20f182
Andre Henn <henn@terrestris.de>
2022-08-16 11:12:02 +0200

    remove exit on non-existing directory

--- start.sh
+++ start.sh
@@ -11,6 +11,6 @@ if [ ! -f /mapproxy/mapproxy.yaml ]
 then
   su "$USER_NAME" -c "mapproxy-util create -t base-config mapproxy"
 fi
-cd /mapproxy || exit
+cd /mapproxy
 su "$USER_NAME" -c "mapproxy-util create -t wsgi-app -f mapproxy.yaml /mapproxy/app.py"
 su "$USER_NAME" -c "uwsgi --ini /uwsgi.conf"
